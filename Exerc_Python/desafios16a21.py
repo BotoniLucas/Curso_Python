a5ecc3331c03ecf8fe19d68380d5e1375b231025
BotoniLucas <68608155+BotoniLucas@users.noreply.github.com>
2020-08-12 13:20:51 -0300

    Update desafios16a21.py

--- Exerc_Python/desafios16a21.py
+++ Exerc_Python/desafios16a21.py
@@ -12,3 +12,23 @@ co = float(input('Digite o cateto oposto: '))
 ca = float(input('Digite o cateto ajdacente: '))
 h = hypot(co, ca)
 print(f'A hipotenusa de {ca} e {co} é {h :.1f}') # nova sintaxe
+
+# desafio 18
+
+from math import sin, cos, tan, radians
+
+ang = float(input('Digite o ângulo: '))
+ang = radians(ang)
+print(f'O seno é {sin(ang) :.2f}, o cosseno é {cos(ang) :.2f} e a tangente é {tan(ang) :.2f}')
+
+# desafio 19
+import random
+
+n1 = input('Digite o nome do primeiro aluno: ')
+n2 = input('Digite o nome do segundo aluno: ')
+n3 = input('Digite o nome do terceiro aluno: ')
+n4 = input('Digite o nome do quarto aluno: ')
+lista = [n1, n2, n3, n4]
+
+ordem = random.choice(lista)
+print(f'O aluno escolhido foi {ordem}')
