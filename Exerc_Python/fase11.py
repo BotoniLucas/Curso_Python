6d6243e65758998d5cdd81b0ad06224784b88cb8
BotoniLucas <68608155+BotoniLucas@users.noreply.github.com>
2020-08-13 11:39:47 -0300

    Create fase11.py

---
+++ Exerc_Python/fase11.py
@@ -0,0 +1,33 @@
+print('\033[1:31mAi ai ai')
+
+print('\033[1:30:44mAi ai ai\033[m') # para tirar o fundo após acabar a palavra (tira todas as formatações)
+
+'''
+Descrição das cores:
+código: \033[
+
+estilos: 0 - nada          (seguido de :)
+         1 - negrito       (seguido de :)
+         4 - sublinhado    (seguido de :)
+         7 - inverte cores (seguido de ;)
+         
+cor do texto (seguida de :):        
+                                30 - branco
+                                31 - vermelho
+                                32 - verde
+                                33 - amarelo
+                                34 - azul
+                                35 - magenta
+                                36 - ciano
+                                37 - cinza (padrão)
+                                
+cor do fundo do texto (seguida de m):        
+                                30 - branco
+                                31 - vermelho
+                                32 - verde
+                                33 - amarelo
+                                34 - azul
+                                35 - magenta
+                                36 - ciano
+                                37 - cinza
+'''
